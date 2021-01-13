e0b065afd96fb59d976fe2aba0c44de7ae1bffb6
jhemesonmotta <jhemesonmotta@gmail.com>
2021-01-13 15:52:16 -0300

    add base do form de add alocacao

--- src/app/components/alocacao/add-alocacao/add-alocacao.component.ts
+++ src/app/components/alocacao/add-alocacao/add-alocacao.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { UsuarioLogado } from 'app/model/usuarioLogado';
+import { SharedService } from 'app/services/shared.service';
+import { SnackBarService } from 'app/services/snackbar/snack-bar.service';
+import { SpinnerService } from 'app/services/spinner.service';
 
 @Component({
   selector: 'app-add-alocacao',
@@ -6,10 +11,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-alocacao.component.css']
 })
 export class AddAlocacaoComponent implements OnInit {
+  usuarioLogado: UsuarioLogado;
 
-  constructor() { }
+  foods: any[] = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'}
+  ];
 
-  ngOnInit(): void {
-  }
+  form = this.fb.group({
+    empresa: new FormControl(null, [Validators.required]),
+    cargo: new FormControl(null, [Validators.required]),
+    dataInicio: new FormControl(null, [Validators.required]),
+    dataFim: new FormControl(null)
+  });
+  
+  constructor (private fb: FormBuilder,
+    private sharedService: SharedService,
+    private spinner: SpinnerService,
+    private snackBarService: SnackBarService) { }
+
+    ngOnInit() {
+      if (this.sharedService.isLoggedIn()) {
+        this.usuarioLogado = this.sharedService.getCurrentLogin();
+      }
+    }
+
+    submit() {
+      this.spinner.showSpinner();
+
+      const formData = this.form.getRawValue();
+      console.log(formData);
+
+      this.spinner.stopSpinner();
+    }
 
 }
