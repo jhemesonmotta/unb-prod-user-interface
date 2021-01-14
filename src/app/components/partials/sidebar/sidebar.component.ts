997cab8f04d0887830c1127e3d628736a2f94d2f
jhemesonmotta <jhemesonmotta@gmail.com>
2021-01-14 15:48:59 -0300

    tirando menu mobile desnecessario

--- src/app/components/partials/sidebar/sidebar.component.ts
+++ src/app/components/partials/sidebar/sidebar.component.ts
@@ -27,10 +27,4 @@ export class SidebarComponent implements OnInit {
   ngOnInit() {
     this.menuItems = ROUTES.filter(menuItem => menuItem);
   }
-  isMobileMenu() {
-      if ($(window).width() > 991) {
-          return false;
-      }
-      return true;
-  };
 }
