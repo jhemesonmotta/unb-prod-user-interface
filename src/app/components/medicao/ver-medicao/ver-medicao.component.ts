e8a698e12820f0546138be4eeca13b5bc3f59292
jhemesonmotta <jhemesonmotta@gmail.com>
2021-01-29 21:08:14 -0300

    base pra tela de ver medição

--- src/app/components/medicao/ver-medicao/ver-medicao.component.ts
+++ src/app/components/medicao/ver-medicao/ver-medicao.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { UsuarioLogado } from 'app/model/usuarioLogado';
+import { SpinnerService } from 'app/services/spinner.service';
+import { SnackBarService } from 'app/services/snackbar/snack-bar.service';
+import { ActivatedRoute } from '@angular/router';
+import { MedicaoService } from 'app/services/medicao/medicao.service';
+import { Medicao } from 'app/model/medicao';
+import { Empresa } from 'app/model/empresa';
+import { EmpresaService } from 'app/services/empresa/empresa.service';
+import { UserService } from 'app/services/usuario/usuario.service';
 
 @Component({
   selector: 'app-ver-medicao',
@@ -7,9 +16,85 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VerMedicaoComponent implements OnInit {
 
-  constructor() { }
+  usuarioLogado: UsuarioLogado;
+  medicao: Medicao;
+  usuarios: Array<UsuarioLogado> = [];
+  empresas: Array<Empresa> = [];
 
-  ngOnInit(): void {
+  constructor(private route: ActivatedRoute,
+    private medicaoService: MedicaoService,
+    private spinner: SpinnerService,
+    private usuarioService: UserService,
+    private empresaService: EmpresaService,
+    private snackBarService: SnackBarService) { }
+
+  ngOnInit() {
+    this.route.params.subscribe(
+      (parametros) => {
+        if(parametros.id != null) {
+          this.buscarMedicao(parametros.id);
+          this.carregarUsuarios();
+          this.carregarEmpresas();
+        }
+      }
+    );
   }
 
+  traduzirUsuario(id: number) {
+    return (id != null && id != 0 && this.usuarios.length > 0) ? this.usuarios.filter(usuario => usuario.id === id)[0].pessoa.nome : '';
+  }
+
+  traduzirEmpresa(id: number) {
+    return (id != null && id != 0 && this.empresas.length > 0) ? this.empresas.filter(empresa => empresa.id === id)[0].nome : '';
+  }
+
+  private buscarMedicao(id: number) {
+    this.spinner.showSpinner();
+
+    this.medicaoService.buscarPorId(id).subscribe((data) => {
+      console.log('data');
+      console.log(data);
+      this.medicao = data;
+      this.spinner.stopSpinner();
+    }, (error) => {
+      console.log('Error: ');
+      console.log(error);
+      this.spinner.stopSpinner();
+      this.snackBarService.erro('Erro ao carregar medição.');
+    });
+  }
+
+  private carregarUsuarios() {
+    this.spinner.showSpinner();
+
+    this.usuarioService.listar().subscribe(
+      (data) => {
+        this.usuarios = data;
+        this.spinner.stopSpinner();
+      }, (error) => {
+        console.log('Error: ');
+        console.log(error);
+
+        this.spinner.stopSpinner();
+        this.snackBarService.erro('Erro ao carregar os usuários.');
+      }
+    );
+  }
+
+  private carregarEmpresas() {
+    this.spinner.showSpinner();
+
+    this.empresaService.listar().subscribe(
+      (data) => {
+        this.empresas = data;
+        this.spinner.stopSpinner();
+      }, (error) => {
+        console.log('Error: ');
+        console.log(error);
+
+        this.spinner.stopSpinner();
+        this.snackBarService.erro('Erro ao carregar as empresas.');
+      }
+    );
+  }
 }
