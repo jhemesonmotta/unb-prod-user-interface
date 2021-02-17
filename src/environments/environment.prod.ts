de9258f0f52a9ff8e4c5a23cf4ddbe1ca05020d9
jhemesonmotta <jhemesonmotta@gmail.com>
2021-02-17 19:33:32 -0300

    atualizando dashboard

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  config: {
+    BACKEND_URL_USUARIOS: "https://unb-ms-usuarios.herokuapp.com/api/v1/",
+    BACKEND_URL_PRODUTIVIDADE: "https://unb-ms-produtividade.herokuapp.com/api/v1/"
+  }
 };
